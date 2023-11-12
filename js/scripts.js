7fd484e9d1629d9bf8a8996c99a8bab02c0352a8
Landon Isaiah <landonisaiah@gmail.com>
2023-11-12 15:01:34 -0800

    Add scripts.js and two buttons with function.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function darkMode() {
+    let buttonOne = document.querySelector("button.button1");
+    buttonOne.style.backgroundColor = "black";
+    buttonOne.style.color = "white";
+}
+
+function lightMode() {
+    let buttonTwo = document.querySelector("button.button2");
+    buttonTwo.style.backgroundColor = "white";
+    buttonTwo.style.color = "black";
+}
