f8bd88a56ed25ca3fb45741d6103b4ea855593f4
Landon Isaiah <landonisaiah@gmail.com>
2023-11-12 19:24:37 -0800

    Add global scope funtion onmouseon and onmouseout with reset in function declarations.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,42 @@
+
+window.onload = function () {
+    let h1TextOn = document.querySelector("h1");
+    h1TextOn.onmouseover = function () {
+        h1TextOn.style.color = "yellow";
+    };
+
+    let h1TextOut = document.querySelector("h1");
+    h1TextOut.onmouseout = function () {
+        h1TextOut.style.color = "black";
+    };
+};
+
 function darkMode() {
+    let h1Text = document.querySelector("h1");
     let buttonOne = document.querySelector("body");
-    let header = document.querySelector("div.header")
+    let header = document.querySelector("div.header");
+    h1Text.style.color = "white"
     buttonOne.style.backgroundColor = "black";
     buttonOne.style.color = "white";
     header.style.borderColor = "white";
-}
+    let h1TextOut = document.querySelector("h1");
+    h1TextOut.onmouseout = function () {
+        h1TextOut.style.color = "white";
+    };
+};
 
 function lightMode() {
+    let h1Text = document.querySelector("h1");
     let buttonTwo = document.querySelector("body");
     let header = document.querySelector("div.header");
+    h1Text.style.color = "black";
     buttonTwo.style.backgroundColor = "white";
     buttonTwo.style.color = "black";
     header.style.borderColor = "black";
-}
+    let h1TextOut = document.querySelector("h1");
+    h1TextOut.onmouseout = function () {
+        h1TextOut.style.color = "black";
+    };
+};
+
+
