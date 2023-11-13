d608690855fd42ee07ad082ba6c8888253d6e914
Landon Isaiah <landonisaiah@gmail.com>
2023-11-12 18:15:09 -0800

    Add dark mode and light mode button functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
 function darkMode() {
-    let buttonOne = document.querySelector("button.button1");
+    let buttonOne = document.querySelector("body");
+    let header = document.querySelector("div.header")
     buttonOne.style.backgroundColor = "black";
     buttonOne.style.color = "white";
+    header.style.borderColor = "white";
 }
 
 function lightMode() {
-    let buttonTwo = document.querySelector("button.button2");
+    let buttonTwo = document.querySelector("body");
+    let header = document.querySelector("div.header");
     buttonTwo.style.backgroundColor = "white";
     buttonTwo.style.color = "black";
+    header.style.borderColor = "black";
 }
